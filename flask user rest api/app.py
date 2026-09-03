b473d6acb6656fa9e702ca87eb15cd3a830ca7b5
muskaancodes-hash <muskaan.kumari7908@gmail.com>
2026-09-03 18:52:43 +0530

    Add files via upload
    Added an in-memory user list and implemented a GET endpoint to retrieve all users in JSON format.

--- flask user rest api/app.py
+++ flask user rest api/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+users = [
+    {"id": 1, "name": "Muskan", "email": "muskan@example.com"},
+    {"id": 2, "name": "Rahul", "email": "rahul@example.com"}
+]
 
 @app.route("/")
 def home():
     return "REST API is running!"
 
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(users)
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
